30e044b31e1dab8da9a00cccce45eaa93375ebda
alisha0217 <69092959+alisha0217@users.noreply.github.com>
2021-03-12 22:15:14 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ function getParagraph1() {
 
 function getParagraph2() {
 
-    var inputs2 = [];
+    var inputs = [];
 
-    for (var i2 = 1; i2 <= 6; i2++) {
+    for (var i = 1; i <= 6; i++) {
 
-        inputs2.push(document.getElementById("para2_input_box_" + i2).value);
+        inputs.push(document.getElementById("para2_input_box_" + i).value);
         
     }
 
-    inputs2.join(". ")
+    inputs.join(". ")
 
-    document.getElementById("para2").innerHTML = inputs2.join(". ");
+    document.getElementById("para2").innerHTML = inputs.join(". ");
 
 }
